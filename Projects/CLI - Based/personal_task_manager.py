882206ae6548f77da6691dbc4670b82ffcae4aff
Somit17 <somitmeshram3256@gmail.com>
2025-12-12 19:27:00 +0530

    Modified logic and completed CLI TODO

--- Projects/CLI - Based/personal_task_manager.py
+++ Projects/CLI - Based/personal_task_manager.py
@@ -3,15 +3,17 @@ import os
 FILE_NAME='tasks.json'
 
 
-def print_Menu():
+def print_menu():
     """Show Main Menu"""
-    print(f'1. Add a task')
-    print(f'2. View all tasks ')
-    print(f'3. Delete a task by id')
-    print(f'0. Exit')
-    print('==============================================')
+    print("====== Personal Task Manager (Todo CLI) ======")
+    print("1. Add a task")
+    print("2. View all tasks")
+    print("3. Mark task as complete")
+    print("4. Delete a task")
+    print("0. Exit")
+    print("==============================================")
 
-def load_Tasks():
+def load_tasks():
      if not os.path.exists(FILE_NAME):
          return []
      with open(FILE_NAME,mode='r') as json_reader:
@@ -21,38 +23,46 @@ def load_Tasks():
              return []
 
 
-def delete_Task(tasks):
-    load_Tasks()
-    idx = input(f'Delete task by id')
+def delete_task(tasks):
+    idx = get_task_index(tasks,'delete')
     if idx is None:
         return
-    deleted_Task = tasks.pop(idx)
+
+    deleted_task = tasks.pop(idx)
     save_tasks(tasks)
-    print(f"Delete task successfully ! Description - {deleted_Task['description']}")
+    print(f"Delete task successfully ! Description - {deleted_task['description']}\n")
+
+
+def mark_task_completed(tasks):
+    idx = get_task_index(tasks,action_name="mark as complete")
+    if idx is None:
+        return
+    tasks[idx]['isCompleted']=True
+    save_tasks(tasks)
+    print(f"Task {idx+1} marked as completed")
+
 
 def main():
-    tasks=load_Tasks()
+    tasks=load_tasks()
     while True:
-        print_Menu()
-        choice = input(f'Choose an option')
+        print_menu()
+        choice = input(f'Choose an option  : - ')
 
-        if choice=='0':
-            print(f'GoodBye')
-            break
-        elif choice=='1':
-            add_Task(tasks)
-            break
+        if choice=='1':
+            add_task(tasks)
         elif choice=='2':
-            view_Tasks(tasks)
-            break
+            view_tasks(tasks)
         elif choice=='3':
-            save_tasks(tasks)
-            break
+            mark_task_completed(tasks)
         elif choice=='4':
-            delete_Task(tasks)
+            delete_task(tasks)
+        elif choice=='0':
+            print("Goodbye! 👋")
             break
+        else:
+            print("Invalid option. Please try again.\n")
 
-def add_Task(tasks):
+def add_task(tasks):
     print('Add a new task with description and priority')
     description = input(f'Please enter the description\n')
     if not  description:
@@ -77,7 +87,7 @@ def save_tasks(tasks):
     with open(FILE_NAME,mode='w') as f:
         json.dump(tasks,f,indent=4)
 
-def view_Tasks(tasks):
+def view_tasks(tasks):
     if not tasks:
         print(f'No Tasks found !\n')
         return
@@ -86,7 +96,22 @@ def view_Tasks(tasks):
         print(f"{i} ) Status - {task['isCompleted']} ** Description  - {task['description']} ** Priority - {task['priority']}\n")
 
 
+def get_task_index(tasks,action_name):
+    if not tasks:
+        print(f'No tasks available.\n')
+        return None
+    view_tasks(tasks)
+    choice = input(f"Enter task number to {action_name} : ")
 
+    if not choice.isdigit():
+        print("Please enter valid number")
+        return  None
+    idx = int(choice) - 1
+
+    if idx < 0 or idx >= len(tasks):
+        print("Invalid task number")
+        return None
+    return  idx
 
 main()
 
