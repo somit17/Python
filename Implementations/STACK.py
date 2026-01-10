6d6c11cb1843f67d4007f89a031f9d7bb31cc6e2
Somit17 <somitmeshram3256@gmail.com>
2026-01-10 23:00:45 +0530

    Stack implementation

---
+++ Implementations/STACK.py
@@ -0,0 +1,26 @@
+#Implementation of stack
+
+
+
+class MyStack :
+    def __init__(self):
+        self.stack = []
+
+    def push(self,item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.stack:
+            raise Exception(f"Stack is empty")
+        return self.stack.pop()
+
+    def peek(self):
+        if not self.stack:
+            raise Exception(f"Stack is empty")
+        return self.stack[-1]
+
+    def is_empty(self):
+        return len(self.stack)==0
+
+    def size(self):
+        return len(self.stack)
